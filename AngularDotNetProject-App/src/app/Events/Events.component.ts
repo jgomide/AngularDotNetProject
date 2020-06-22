9869d275b83121929b2db4fff676a1e70a5ec67b
Gomide <jefersongomide@gmail.com>
2020-06-22 15:24:38 +1000

    deleting vscode folder

--- AngularDotNetProject-App/src/app/Events/Events.component.ts
+++ AngularDotNetProject-App/src/app/Events/Events.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
-  selector: 'app-Events',
+  selector: 'app-events',
   templateUrl: './Events.component.html',
   styleUrls: ['./Events.component.css']
 })
